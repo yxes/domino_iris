3dc8258771d25a24cb9c08e4665ffdefb67e21c9
Steve Wells <yxes123@gmail.com>
2015-09-30 21:37:24 -0700

    Code Added to Presentation / Added Documentation
    * Added Code Examples to Presentation
    * Python Library Requirements Added
    * Fixed Minor Typo in Presentation
    * Templates now switch between basic and bootstrap versions
    * Updated README.md with Additional Documentation and Credits

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # domino_iris
 Simple example of using a domino-based website.
 
+## View it on the Web @ heroku
+
+(https://immense-coast-7696.herokuapp.com/)[Iris Prediction]
+
+*...when you get there - click "Input Form" to start ...*
+
+## INSTALL
+
+the required python libraries are housed in a standard
+requirements.txt file - to install them just use:
+
+```{bash, echo=FALSE}
+pip install -r requirements.txt
+```
+
+Be sure to add your Domino API Key to your environment (see below)
+
+and then just run:
+
+```{bash, echo=FALSE}
+python flask_iris.py
+```
+
 ## Domino API Key
 
 You'll need to use your own domino inspired api key. First
@@ -12,14 +35,47 @@ where you will find your own personal API key. Copy it.
 To run these commands you'll want to set the environment variable:
 DOMINO_API_KEY before they'll work.
 
-```{bash}
+```{bash, echo=FALSE}
 export DOMINO_API_KEY=<YOUR API KEY>
 ```
 
+on windows you'll need to use the **set** command
+
+```{bash, echo=FALSE}
+set DOMINO_API_KEY=<YOUR API KEY>
+```
+
 ## Included Files
+
+**Presentation Software**
+
+* deploy-model.Rmd - ioslides presentation
+* deploy-model.html - in html format
+* image - directory to hold... um... something...
+* domino_iris.Rproj - project deployment
+
+**Executables**
 
 * cli.sh - shell script example for making domino connection
 * flask_iris.py - python script for running a simple website
 * templates/ - directory for housing jinja2 templates
 * static/ - static files for flask
+* requirements.txt - python library requirements
 
+## Authors
+
+**Daniel Emaasit** (Ph.D. Student)
+
+Use R for statistical programming in my research to solve Transportation Engineering problems like crashes, congestion e.t.c.
+
+* Github: [https://github.com/Emaasit](https://github.com/Emaasit)
+* Email: [daniel.emaasit@gmail.com](mailto:daniel.emaasit@gmail.com)
+* Website: [http://www.danielemaasit.com](http://www.danielemaasit.com)
+
+**Steve Wells** (C.T.O, Cumulus)  
+
+Computer Programmer & Data Scientist
+
+* Github: [https://github.com/yxes](https://github.com/yxes)
+* Email: [yxes@cpan.org](mailto:yxes@cpan.org)
+* Website: [http://www.stephendwells.com/](http://www.stephendwells.com/)
--- flask_iris.py
+++ flask_iris.py
@@ -1,41 +1,82 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request
-import os
-import requests
-import json
 
-# configuration
-DEBUG = True
+"""
+
+Author: Stephen D. Wells <wells@cedarnet.org>
+Date: 9/31/15
+Version: 0.01 - currently no error checking / debug
+
+Runs a simple webserver that passes values from
+a form to Domino Data Labs via their API. The 
+return value is then sent to the template for
+rendering.
+
+"""
+# EXTERNAL LIBRARIES
+
+# Flask is our Web Framework in this case
+from flask import Flask, render_template, request
+import os # grab the ENV variable
+import requests # make Domino API Request
+import json # parse Domino's results
+
+# CONFIGURATION
+
+# before running this program please ensure
+# that you've setup your Domino API Key
+# from http://dominodatalab.com/
+
+# export DOMINO_API_KEY='xA3f...Syds'
+
 API_KEY = os.environ['DOMINO_API_KEY']
+
+# This is a list of keys your API requires.
+# The order of these variables is important.
+# You have been warned.
 MODEL_ATTRS = [
-        'sepal_length',
-        'sepal_width',
-        'petal_length',
-        'petal_width'
+    'sepal_length',
+    'sepal_width',
+    'petal_length',
+    'petal_width'
 ]
 
+# Fire up our Flask Object
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config.from_envvar('FLASK_SETTINGS', silent=True)
+# If you'd rather load from a config file...
+#   http://flask.pocoo.org/docs/0.10/tutorial/setup/
+#app.config.from_envvar('FLASK_SETTINGS', silent=True)
 
+# INDEX PAGE - all the magic happens here
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    error = None
-    result = None
-    if request.method == 'POST':
-        attributes = []
+    error = None # we don't use this... but we should eventually so I left it
+    result = None # the json variable we get back from Domino
+
+    if request.method == 'POST': # you submitted the form
+
+        attributes = [] # just the values from the form in an array
         for field in MODEL_ATTRS:
             attributes.append(request.form[field])
+
         result = fetch_score(attributes)
         result = result.json()
-        if result is not None:
+
+        if result is not None: # notice that we don't do anything if it didn't work
+                               # we should eventually - consider this a placeholder
             print("result:", result['result'])
+
+        # since we got a result we pass it to the template for rendering
+        #    once again - error is always None at this stage
         return render_template('index.html', fields=MODEL_ATTRS, error=error, result=result)
 
+    # by not returning result it passes to the template as not defined - we can use that!
     return render_template('index.html', fields=MODEL_ATTRS)
 
-# you'll want to have your attributes formatted in the right order
+# DOMINO API REQUEST
+
+# you'll want to have your attributes formatted in the right order - see above
 def fetch_score(attributes):
     return requests.post("https://app.dominodatalab.com/v1/SparkIQLabs/helloWorld/endpoint",
                 headers = {
@@ -45,6 +86,10 @@ def fetch_score(attributes):
                 json={ "parameters": attributes }
     )
 
+# START YOUR ENGINES!
+
+# This allows you to use this script as a library instead of an executable
+#  which though highly unlikely is good practice...
 if __name__ == '__main__':
     app.run()
 
